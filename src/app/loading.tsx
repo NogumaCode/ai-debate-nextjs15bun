382126b280e0c81c6997f64fb3792dbdc9bcc47c
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-11-03 01:57:33 +0900

    db setting

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import SpinnerModal from '@/components/SpinnerModal'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <SpinnerModal/>
+  )
+}
+
+export default loading
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  // エラーページ検証
+  // throw new Error("This is a server-side error for testing the error page.");
+  //ローディング画面検証
+  // await new Promise((resolve)=>setTimeout(resolve,6000))
   return (
     <div></div>
   );
